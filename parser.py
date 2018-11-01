be58fe035c387e4726552a1bfcf3529474c5fbae
Jan Gaura <jan.gaura@gmail.com>
2018-11-01 13:20:10 +0100

    Parse different files passed from cmd line

--- parser.py
+++ parser.py
@@ -1,13 +1,16 @@
+import argparse
 import html5lib
 from collections import OrderedDict
+import sys
 
 TABLE = '{http://www.w3.org/1999/xhtml}table'
 TBODY = '{http://www.w3.org/1999/xhtml}tbody'
 TR = '{http://www.w3.org/1999/xhtml}tr'
 TD = '{http://www.w3.org/1999/xhtml}td'
 
-sh = OrderedDict(sorted({0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}.items(), key=lambda t: t))
+DAYS = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota']
 
+sh = OrderedDict(sorted({0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}.items(), key=lambda t: t))
 
 def parse_lecture(td):
     hours = int(td.attrib['colspan'])
@@ -61,43 +64,78 @@ def print_schedule(sh):
             print(hour, end=' ')
         print()
 
-    
-with open('timetable.html', 'rb') as f:
-    doc = html5lib.parse(f)
-    print(doc)
-    body = doc.find('{http://www.w3.org/1999/xhtml}body')
-    print(body)
-    print()
-    for e in body:
-        if e.tag == '{http://www.w3.org/1999/xhtml}table':
-            #print(e)
-            #print(e.attrib)
-            cl = e.attrib['class']
-            if cl == 'grid-border-args':
-                tbody = e.find('{http://www.w3.org/1999/xhtml}tbody')
-                for day_no, row in enumerate(tbody.findall('{http://www.w3.org/1999/xhtml}tr')):
-                    #print(row)
-                    # for each time slot
-                    for td in row.findall('{http://www.w3.org/1999/xhtml}td'):
-                        if 'class' in td.attrib:
-                            cl = td.attrib['class']
-                            if cl == 'col-label-one' or cl == 'row-label-one':
-                                text = td.text if td.text else '       '
-                                print(td.text, end=' ')
-                            # actial lecture
-                            elif cl == 'object-cell-border':
-                                #print('HODINA')
-                                hours, lecture = parse_lecture(td)
-                                for i in range(hours):
-                                    sh[day_no].append('X')
-                            else:
-                                # empty
-                                sh[day_no].append('O')
-                        else:
-                            print('       ')
-                    print()
 
-print_schedule(sh)
+def main():
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('filename', metavar='filename', type=str, nargs='+',
+                        help='filename with HTML timetable')
+    
+    args = parser.parse_args()
+    print(args.filename)
+
+    hours_list = []
+
+    with open(args.filename[0], 'rb') as f:
+        doc = html5lib.parse(f)
+        print(doc)
+        body = doc.find('{http://www.w3.org/1999/xhtml}body')
+        print(body)
+        print()
+        for e in body:
+            if e.tag == '{http://www.w3.org/1999/xhtml}table':
+                #print(e)
+                #print(e.attrib)
+                cl = e.attrib['class']
+                if cl == 'grid-border-args':
+                    tbody = e.find('{http://www.w3.org/1999/xhtml}tbody')
+                    for day_no, row in enumerate(tbody.findall('{http://www.w3.org/1999/xhtml}tr')):
+                        #print(row)
+                        # for each time slot
+                        for col_no, td in enumerate(row.findall('{http://www.w3.org/1999/xhtml}td')):
+                            #print(td.text)
+                            #print(sh)
+                            if 'class' in td.attrib:
+                                cl = td.attrib['class']
+                                if cl == 'col-label-one' or cl == 'row-label-one':
+                                    text = td.text if td.text else '       '
+                                    if text in DAYS:
+                                        max_day_len = max([ len(s) for s in DAYS])
+                                        print(td.text + ''.rjust(max_day_len - len(text)) + ': ', end=' ')
+                                    else:
+                                        hours_list.append(td.text)
+                                        print(td.text, end=' ')
+                                # actual lecture
+                                elif cl == 'object-cell-border':
+                                    #print('HODINA')
+                                    hours, lecture = parse_lecture(td)
+                                    inner_col_no = col_no
+                                    for i in range(hours):
+                                        if inner_col_no < 5:
+                                            sh[day_no].append('X'.rjust(5) + ''.ljust(4))
+                                        else:
+                                            sh[day_no].append('X'.rjust(7) + ''.ljust(6))
+                                        inner_col_no += 1
+                                else:
+                                    # empty
+                                    if col_no < 5:
+                                        sh[day_no].append('O'.rjust(5) + ''.ljust(4))
+                                    else:
+                                        sh[day_no].append('O'.rjust(7) + ''.ljust(6))
+                            else:
+                                print('       ')
+                        print()
+
+    print()
+
+    for hour in hours_list:
+        print(hour, end=' ')
+    print_schedule(sh)
+
+
+if __name__ == '__main__':
+    main()
+
 
 #import requests
 
